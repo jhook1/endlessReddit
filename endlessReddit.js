f08f972a9067651e652839052cd6677c287063c2
John Hook <jhook1@ufl.edu>
2022-02-20 14:41:39 -0500

    Added scroll listener

--- endlessReddit.js
+++ endlessReddit.js
@@ -1,16 +1,16 @@
 function getNextUrl(){
-    var nextBtn = document.querySelector(".nav-buttons .next-button a");
-    var nextPageUrl = nextBtn.getAttribute("href");
+    const nextBtn = document.querySelector(".nav-buttons .next-button a");
+    const nextPageUrl = nextBtn.getAttribute("href");
     console.log("next:",nextPageUrl);
     return nextPageUrl;
 }
 
 function fetchNextPage(){
     fetchCt++;
-    if(fetchCt>5){console.log("maxed");return;}
+    if(fetchCt>=5){console.log("maxed");return;}
 
     console.log("fetching...");
-    var textResult = "";
+    const textResult = "";
     // fetch returns a promise which resolves to a response whose body is a ReadableStream
     fetch(getNextUrl()).then(res=>res.body).then(bod=>handleFetchResponse(bod,textResult));
 }
@@ -18,8 +18,8 @@ function fetchNextPage(){
 function handleFetchResponse(resp,textBuff){
     console.log("fetched");
 
-    var decoder = new TextDecoder();
-    var reader = resp.getReader();
+    const decoder = new TextDecoder();
+    const reader = resp.getReader();
     reader.read().then(function readTextFromStream({done,value}){ // {done,value} required param names
         if(done){
             console.log("read");
@@ -27,7 +27,7 @@ function handleFetchResponse(resp,textBuff){
             return;
         }
 
-        var decodedText = decoder.decode(value);
+        const decodedText = decoder.decode(value);
         textBuff += decodedText;
 
         return reader.read().then(readTextFromStream);
@@ -35,24 +35,27 @@ function handleFetchResponse(resp,textBuff){
 }
 
 function appendNextSiteTable(text){
-    var prevSiteTable = document.querySelector(`.siteTable-page${fetchCt}`);
-    var navBtns = prevSiteTable.querySelector(".nav-buttons");
+    const prevSiteTable = document.querySelector(`.siteTable-page${fetchCt}`);
+    const navBtns = prevSiteTable.querySelector(".nav-buttons");
     navBtns.remove();
 
-    var parser = new DOMParser();
-    var nextPageDoc = parser.parseFromString(text,"text/html");
-    var nextSiteTable = nextPageDoc.getElementById("siteTable");
+    const parser = new DOMParser();
+    const nextPageDoc = parser.parseFromString(text,"text/html");
+    const nextSiteTable = nextPageDoc.getElementById("siteTable");
     nextSiteTable.classList.add(`siteTable-page${fetchCt+1}`);
 
     prevSiteTable.after(nextSiteTable);
     console.log("appended");
 
-    attachEventHandlers(nextSiteTable);
+    registerEventHandlers(nextSiteTable);
+    console.log("registered")
+
+    scrollLocked = false;
 }
 
-function attachEventHandlers(siteEl){
+function registerEventHandlers(siteEl){
     $(siteEl).on("click",".expando-button",(e)=>{
-        var thingy = $(e.target).closest(".thing");
+        const thingy = $(e.target).closest(".thing");
         if(thingy.data("expando")) return;
         thingy.data("expando",true);
         new ExpandoController({
@@ -62,18 +65,18 @@ function attachEventHandlers(siteEl){
     });
 }
 
-var ExpandoController = Backbone.View.extend({
+const ExpandoController = Backbone.View.extend({
     buttonSelector: '.expando-button',
     expandoSelector: '.expando',
     expanded: false,
 
     events: {
-      'click .expando-button': 'toggleExpando'
+        'click .expando-button': 'toggleExpando'
     },
 
     constructor: function() {
-      Backbone.View.prototype.constructor.apply(this, _.toArray(arguments));
-      this.toggleExpando();
+        Backbone.View.prototype.constructor.apply(this, _.toArray(arguments));
+        this.toggleExpando();
     },
 
     initialize: function() {
@@ -88,60 +91,60 @@ var ExpandoController = Backbone.View.extend({
         this.autoexpanded = this.options.autoexpanded;
   
         if (this.autoexpanded) {
-          this.loaded = true;
-          this.cachedHTML = this.$expando.html();
+            this.loaded = true;
+            this.cachedHTML = this.$expando.html();
         }
   
-        var $e = $.Event('expando:create', { expando: this });
+        const $e = $.Event('expando:create', { expando: this });
         $(document.body).trigger($e);
   
         if ($e.isDefaultPrevented()) { return; }
   
         $(document).on('hide_thing_' + this.id, function() {
-          this.toggleExpando()
+            this.toggleExpando()
         }.bind(this));
   
         // expando events
-        var linkURL = this.$el.children('.entry').find('a.title').attr('href');
+        let linkURL = this.$el.children('.entry').find('a.title').attr('href');
   
         if (/^\//.test(linkURL)) {
-          var protocol = window.location.protocol;
-          var hostname = window.location.hostname;
-          linkURL = protocol + '//' + hostname + linkURL;
+            const protocol = window.location.protocol;
+            const hostname = window.location.hostname;
+            linkURL = protocol + '//' + hostname + linkURL;
         }
   
         // event context
-        var eventData = {
-          linkIsNSFW: this.isNSFW,
-          linkType: this.linkType,
-          linkURL: linkURL,
+        const eventData = {
+            linkIsNSFW: this.isNSFW,
+            linkType: this.linkType,
+            linkURL: linkURL
         };
         
         // note that hyphenated data attributes will be converted to camelCase
-        var thingData = this.$el.data();
+        const thingData = this.$el.data();
   
         if ('fullname' in thingData) {
-          eventData.linkFullname = thingData.fullname;
+            eventData.linkFullname = thingData.fullname;
         }
   
         if ('timestamp' in thingData) {
-          eventData.linkCreated = thingData.timestamp;
+            eventData.linkCreated = thingData.timestamp;
         }
   
         if ('domain' in thingData) {
-          eventData.linkDomain = thingData.domain;
+            eventData.linkDomain = thingData.domain;
         }
   
         if ('authorFullname' in thingData) {
-          eventData.authorFullname = thingData.authorFullname;
+            eventData.authorFullname = thingData.authorFullname;
         }
   
         if ('subreddit' in thingData) {
-          eventData.subredditName = thingData.subreddit;
+            eventData.subredditName = thingData.subreddit;
         }
   
         if ('subredditFullname' in thingData) {
-          eventData.subredditFullname = thingData.subredditFullname;
+            eventData.subredditFullname = thingData.subredditFullname;
         }
   
         this._expandoEventData = eventData;
@@ -155,32 +158,32 @@ var ExpandoController = Backbone.View.extend({
 
     show: function() {
         if (!this.loaded) {
-          return $.request('expando', { link_id: this.id }, function(res) {
-            var expandoHTML = $.unsafe(res);
-            this.cachedHTML = expandoHTML;
-            this.loaded = true;
-            this.show();
-          }.bind(this), false, 'html', true);
+            return $.request('expando', { link_id: this.id }, function(res) {
+                const expandoHTML = $.unsafe(res);
+                this.cachedHTML = expandoHTML;
+                this.loaded = true;
+                this.show();
+            }.bind(this), false, 'html', true);
         }
   
-        var $e = $.Event('expando:show', { expando: this });
+        const $e = $.Event('expando:show', { expando: this });
         this.$el.trigger($e); //
   
         if ($e.isDefaultPrevented()) { return; }
   
         if (!this.autoexpanded) {
-          this.$expando.html(this.cachedHTML);
+            this.$expando.html(this.cachedHTML);
         }
   
         if (!this._expandoEventData.provider) {
-          // this needs to be deferred until the actual embed markup is available.
-          var $media = this.$expando.children();
-  
-          if ($media.is('iframe')) {
-            this._expandoEventData.provider = 'embedly';
-          } else {
-            this._expandoEventData.provider = 'reddit';
-          }
+            // this needs to be deferred until the actual embed markup is available.
+            const $media = this.$expando.children();
+    
+            if ($media.is('iframe')) {
+                this._expandoEventData.provider = 'embedly';
+            } else {
+                this._expandoEventData.provider = 'reddit';
+            }
         }
   
         this.$expando.removeClass('expando-uninitialized');
@@ -193,9 +196,17 @@ var ExpandoController = Backbone.View.extend({
     },
 });
 
-var fetchCt = 0;
 document.getElementById("siteTable").classList.add("siteTable-page1");
-fetchNextPage();
+
+let fetchCt = 0;
+let scrollLocked = false;
+window.addEventListener("scroll",()=>{
+    if(scrollLocked) return;
+    if(window.scrollMaxY - window.scrollY < .5 * document.getElementById("siteTable").clientHeight){
+        scrollLocked = true;
+        fetchNextPage();
+    }
+});
 
 
 //https://github.com/reddit-archive/reddit/blob/master/r2/r2/public/static/js/expando.js
